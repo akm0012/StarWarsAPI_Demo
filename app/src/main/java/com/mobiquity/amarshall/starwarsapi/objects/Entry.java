dfbb2c7ee441dd5d3d3dedc771dccd81b21d3196
Andrew Marshall <amarshall@mobiquityinc.com>
2015-07-20 16:26:06 -0400

    Added Entry Class

---
+++ app/src/main/java/com/mobiquity/amarshall/starwarsapi/objects/Entry.java
@@ -0,0 +1,109 @@
+package com.mobiquity.amarshall.starwarsapi.objects;
+
+import java.io.Serializable;
+
+public class Entry implements Serializable {
+
+    private String mName;
+    private String mHeight;
+    private String mMass;
+    private String mHairColor;
+    private String mSkinColor;
+    private String mEyeColor;
+    private String mBirthYear;
+    private String mGender;
+    private String mURL;
+
+    public Entry(String _name, String _height, String _mass,
+                 String _hairColor, String _skinColor,
+                 String _eyeColor, String _birthYear,
+                 String _gender, String _URL) {
+
+
+        mName = _name;
+        mHeight = _height;
+        mMass = _mass;
+        mHairColor = _hairColor;
+        mSkinColor = _skinColor;
+        mEyeColor = _eyeColor;
+        mBirthYear = _birthYear;
+        mGender = _gender;
+        mURL = _URL;
+
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmHeight() {
+        return mHeight;
+    }
+
+    public void setmHeight(String mHeight) {
+        this.mHeight = mHeight;
+    }
+
+    public String getmMass() {
+        return mMass;
+    }
+
+    public void setmMass(String mMass) {
+        this.mMass = mMass;
+    }
+
+    public String getmHairColor() {
+        return mHairColor;
+    }
+
+    public void setmHairColor(String mHairColor) {
+        this.mHairColor = mHairColor;
+    }
+
+    public String getmSkinColor() {
+        return mSkinColor;
+    }
+
+    public void setmSkinColor(String mSkinColor) {
+        this.mSkinColor = mSkinColor;
+    }
+
+    public String getmEyeColor() {
+        return mEyeColor;
+    }
+
+    public void setmEyeColor(String mEyeColor) {
+        this.mEyeColor = mEyeColor;
+    }
+
+    public String getmBirthYear() {
+        return mBirthYear;
+    }
+
+    public void setmBirthYear(String mBirthYear) {
+        this.mBirthYear = mBirthYear;
+    }
+
+    public String getmGender() {
+        return mGender;
+    }
+
+    public void setmGender(String mGender) {
+        this.mGender = mGender;
+    }
+
+    public String getmURL() {
+        return mURL;
+    }
+
+    public void setmURL(String mURL) {
+        this.mURL = mURL;
+    }
+
+
+
+}
